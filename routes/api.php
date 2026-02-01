ce8b930e41ed7aa53756b3b6d170efa54a1977ab
Gabriel Capili <41713299+gabbbcapili@users.noreply.github.com>
2026-02-01 22:37:34 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::get('/applications', [ApplicationController::class, 'index']);
-// });
+});
